7d76c507ec4611cc05ebb59482daadb312fd91f7
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-09 10:49:18 -0400

    used useEffect to fetch ale beer list

---
+++ src/Components/Beers/index.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function Beers() {
+  const [beerList, setBeerList] = useState();
+  useEffect(() => {
+    fetch(`https://api.sampleapis.com/beers/ale`) //ale, stouts, red-ale
+      .then((response) => response.json())
+      .then((beers) => setBeerList(beers))
+      .catch(alert);
+  }, []);
+  if (!beerList) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <>
+      <ul>
+        {beerList.map((beer) => (
+          <li key={beer.id}>{beer.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
